311b93cc29e9b13f77c5ce34187c65d1c2310214
holrock <ispeporez@gmail.com>
2015-08-09 10:43:11 +0900

    test search results

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -3,8 +3,24 @@ app.controller('CustomerSearchController',
     [
       '$scope', 
       function($scope) {
+        $scope.customers = []
         $scope.search = function(searchTerm) {
-          $scope.searchedFor = searchTerm;
+          $scope.customers = [
+            {
+              'first_name': 'Schuyler',
+              'last_name': 'Cremin',
+              'email': 'glies0@macgyver.net',
+              'username': 'jillian0',
+              'created_at': '2015-03-04',
+            },
+            {
+              'first_name': 'hoge',
+              'last_name': 'foo',
+              'email': 'bar@macgyver.net',
+              'username': 'quuuq',
+              'created_at': '2015-03-05',
+            },
+          ];
         }
       }
     ]
