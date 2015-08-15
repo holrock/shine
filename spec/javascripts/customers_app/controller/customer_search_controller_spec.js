1600fa77f890047f0543064f3cd25706717efa1e
holrock <ispeporez@gmail.com>
2015-08-15 18:24:02 +0900

    anguler tests

---
+++ spec/javascripts/customers_app/controller/customer_search_controller_spec.js
@@ -0,0 +1,24 @@
+describe('CustomerSearchController', function() {
+  describe('Initialization', function() {
+
+    var scope      = null,
+        controller = null;
+
+    beforeEach(module('customers'));
+    /*
+
+    beforeEach(inject(function($controller, $rootScope) {
+      scope = $rootScope.$new();
+      controller = $controller('CustomerSearchController', {
+        $scope: scope
+      });
+    });
+    */
+
+    // tests go hear..
+    /*(it('defaults to an empty customer list', function() {
+      expect(scope.customers).toBe([]);
+    });
+    */
+  });
+});
